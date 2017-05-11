a13c9e74844200b120e47e19b53113650e695c77
jacace <javierandrescaceres@gmail.com>
2017-05-11 15:28:29 +0100

    Added Spring saple pages

---
+++ src/main/java/com/jacace/popUpBlocker/controller/PopUpBlockerController.java
@@ -0,0 +1,62 @@
+package com.jacace.popUpBlocker.controller;
+
+import java.io.IOException;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class PopUpBlockerController {
+
+	private static int counter = 0;
+	private static final String VIEW_INDEX = "index";
+	private final static org.slf4j.Logger logger = LoggerFactory.getLogger(BaseController.class);
+
+	//Inspired in: https://www.mkyong.com/maven/how-to-create-a-web-application-project-with-maven/
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String welcome(ModelMap model) {
+
+		model.addAttribute("message", "Welcome");
+		model.addAttribute("counter", ++counter);
+		logger.debug("[welcome] counter : {}", counter);
+
+		// Spring uses InternalResourceViewResolver and return back index.jsp
+		return VIEW_INDEX;
+
+	}
+
+	@RequestMapping(value = "/{name}", method = RequestMethod.GET)
+	public String welcomeName(@PathVariable String name, ModelMap model) {
+
+		//String url=request.getParameter("url");
+		//PrintWriter out =response.getWriter();
+		//out.println("The url is: " + url);
+		
+		model.addAttribute("message", "Welcome " + name);
+		model.addAttribute("counter", ++counter);
+		logger.debug("[welcomeName] counter : {}", counter);
+		return VIEW_INDEX;
+
+	}
+
+	/*
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	// @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String url=request.getParameter("url");
+		PrintWriter out =response.getWriter();
+		out.println("The url is: " + url);
+	}	
+	*/
+
+}
